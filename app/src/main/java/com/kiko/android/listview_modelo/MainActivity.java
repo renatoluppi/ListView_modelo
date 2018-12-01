7692d4604d2c14776cffa1961c3dc50026cb76f2
Renato Luppi <renatoluppi@gmail.com>
2018-12-01 08:42:23 -0200

    Initial commit

---
+++ app/src/main/java/com/kiko/android/listview_modelo/MainActivity.java
@@ -0,0 +1,46 @@
+package com.kiko.android.listview_modelo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+// TODO: 01/12/18 subir no github!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+
+    private ListView listaPraias;
+    private String[] praias = {
+            "Santos" , "Praia Grande", "Monguaguá", "Florianópolis", "Natal", "Caxias",
+            "Interlagos", "Vila Maria", "Brooklin" , "São Pedro", "Monaco", "Indonésia", "Paris", "Italia", "Brasil"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listaPraias = (ListView) findViewById(R.id.listaPraiasId);
+
+        ArrayAdapter<String> benjamim = new ArrayAdapter<String>(
+                getApplicationContext(),android.R.layout.simple_list_item_1, android.R.id.text1, praias);
+
+        listaPraias.setAdapter(benjamim);
+
+        listaPraias.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                int codigoPosicao = i;
+                String valorClicado = listaPraias.getItemAtPosition(codigoPosicao).toString();
+                Toast.makeText(getApplicationContext(), valorClicado, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+
+    
+}
